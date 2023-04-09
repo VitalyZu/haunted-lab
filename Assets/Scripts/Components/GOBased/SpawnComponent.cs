63b5c36feaad646b059d27966978ccb670730be0
VitalyZu <vitaly.z.vz@gmail.com>
2023-04-09 23:09:46 +0300

    add-pool-objects-spawn-component

---
+++ Assets/Scripts/Components/GOBased/SpawnComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnComponent : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private bool _usePool;
+
+    [ContextMenu("Spawn")]
+    public void Spawn()
+    {
+        GameObject instance = _usePool ?
+            Pool.Instance.Get(_prefab, _target.position) :
+            SpawnUtil.Spawn(_prefab, _target.position);
+
+        instance.transform.localScale = _target.lossyScale;
+    }
+
+    public void SetPrefab(GameObject prefab)
+    {
+        _prefab = prefab;
+    }
+}
---
+++ Assets/Scripts/Utils/PoolObjects/PoolItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PoolItem : MonoBehaviour
+{
+    private int _id;
+    private Pool _pool;
+    [SerializeField] private UnityEvent _onReload;
+
+    public void Retain(int id, Pool pool)
+    {
+        _id = id;
+        _pool = pool;
+    }
+
+    public void Release()
+    {
+        _pool?.Release(_id, this);
+    }
+
+    public void Restart()
+    {
+        _onReload?.Invoke();
+    }
+}
