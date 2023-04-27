46758fb14bd32fea735077a09fa82891abd06507
VitalyZu <vitaly.z.vz@gmail.com>
2023-04-27 10:46:38 +0300

    Update CheckCircleOverlap.cs

--- Assets/Scripts/Components/ColliderBased/CheckCircleOverlap.cs
+++ Assets/Scripts/Components/ColliderBased/CheckCircleOverlap.cs
@@ -11,17 +11,31 @@ public class CheckCircleOverlap : MonoBehaviour
     [SerializeField] OnOverlapEvent _onOverlap;
     [SerializeField] private LayerMask _mask;
     [SerializeField] private string[] _tags;
-    private Collider2D[] _interactResult = new Collider2D[10];
+    private List<Collider2D> _interactResult = new List<Collider2D>();
 
     public void Check()
     {
-        int hit = Physics2D.OverlapCircleNonAlloc(
+        Physics2D.OverlapCircle(
             transform.position,
             _radius,
-            _interactResult,
-            _mask);
+             new ContactFilter2D().NoFilter(),
+            _interactResult);
 
-        for (int i = 0; i < hit; i++)
+        foreach (var item in _interactResult)
+        {
+            bool isCompare = false;
+            foreach (var tag in _tags)
+            {
+                if (item.gameObject.CompareTag(tag))
+                {
+                    isCompare = true;
+                }
+            }
+
+            if (isCompare) _onOverlap?.Invoke(item.gameObject);
+        }
+
+        /*for (int i = 0; i < hit; i++)
         {
             bool isCompare = false;
             foreach (var tag in _tags)
@@ -33,7 +47,7 @@ public class CheckCircleOverlap : MonoBehaviour
             }
 
             if (isCompare) _onOverlap?.Invoke(_interactResult[i].gameObject);
-        }
+        }*/
     }
 
 #if UNITY_EDITOR
